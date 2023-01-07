dae859c3aea25ba5d6b612121e839f2fa66a9631
Kutyrev Dmitry <turbosuns@yandex.ru>
2023-01-07 20:22:02 +0300

    esc menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject menu;
+    private bool visible = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            visible = !visible;
+            menu.SetActive(visible);
+        }
     }
 
     public void NewGame()
